47cbb8359cdd230456ca9781d08378c15d3794c7
LREY68 <laurareynolds68@hotmail.com>
2022-08-27 11:41:33 -0500

    Initial website testing

---
+++ index.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html>
+  <head>
+  <meta name="viewport" content="width=device-width, initial-scale=1", charset="utf-8">
+    <link rel="stylesheet" href="lrstyle.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">  
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script> 
+  
+    <title >Larry Whitehouse</title>
+
+    <style>
+        header {
+            animation: fadeInAnimation ease 3s;
+            animation-iteration-count: 1;
+            animation-fill-mode: forwards;
+        }
+        @keyframes fadeInAnimation {
+            0% {
+                opacity: 0;
+            }
+            100% {
+                opacity: 1;
+            }
+        }
+    </style>
+  </head>
+ 
+  <div class="header">
+  <h2>Larry Whitehouse</h2>
+</div>
+
+<div class="row">
+  <div class="column" style="background-color:#aaa;">Angel</div>
+  <div class="column" style="background-color:#bbb;">Column</div>
+  <div class="column" style="background-color:#ccc;">Column</div>
+</div>
+
+<div class="footer">
+  <p>Footer</p>
+</div>
+
+</body>
+</html>
