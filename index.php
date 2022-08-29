9415c101910ad7992a9d7394f2a251e3132cb7ac
LREY68 <106503484+LREY68@users.noreply.github.com>
2022-08-28 21:11:50 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
   <head>
   <meta name="viewport" content="width=device-width, initial-scale=1", charset="utf-8">
-    <link rel="stylesheet" href="lrstyle.css">
+    <link rel="stylesheet" href="mystyle.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">  
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
